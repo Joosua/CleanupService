f9bccf8f72d86be56a951635ace14ea3c1d903ce
pixtm <teemu.vaisanen@ludocraft.com>
2015-01-25 00:53:18 +0200

    Added various art assets. added water

---
+++ Assets/Scripts/BoyancyVolume.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoyancyVolume : MonoBehaviour {
+	public Transform waterlevel;
+	void OnTriggerStay(Collider other) {
+
+		if (other.attachedRigidbody && waterlevel){
+			float depth =  waterlevel.position.y - other.attachedRigidbody.transform.position.y;
+			other.attachedRigidbody.AddForce(Vector3.up * (6.0f + 15.0f*depth));
+		}
+	}
+}

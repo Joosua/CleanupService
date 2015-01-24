589fd746125c861ae3be8f1d2daa3f1b85cc6e49
pixtm <teemu.vaisanen@ludocraft.com>
2015-01-24 21:10:53 +0200

    Added more blood and removing logic

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+	Vector3 lastpos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(1)){
+			lastpos=Input.mousePosition;
+		}
+		if(Input.GetMouseButton(1)){
+			Vector3 delta = Input.mousePosition - lastpos;
+			transform.position -= delta * 0.02f;
+			lastpos=Input.mousePosition;
+		}
+	}
+}

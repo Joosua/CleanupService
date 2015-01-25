db3a0d477eb403e6a63339217e512c8dbd30c687
pixtm <teemu.vaisanen@ludocraft.com>
2015-01-25 04:21:14 +0200

    added art

--- Assets/Scripts/BloodStain.cs
+++ Assets/Scripts/BloodStain.cs
@@ -4,14 +4,15 @@ using System.Collections;
 public class BloodStain : MonoBehaviour {
 	float amount;
 	public float maxblood = 100f;
-	Material material;
+	//Material mat;
 	public float startcutout;
 	// Use this for initialization
 	void Start () {
 		amount = 0;
-		material = new Material (renderer.material);
-		renderer.material = material;
-		startcutout = material.GetFloat ("_Cutoff");
+		//mat = new Material (renderer.material);
+		startcutout = renderer.material.GetFloat ("_Cutoff");
+		//renderer.material = mat;
+
 	}
 	
 	void OnTriggerEnter(Collider other) {
@@ -29,13 +30,15 @@ public class BloodStain : MonoBehaviour {
 	}
 
 	void RemoveBlood(float removeamount){
+
 		amount += removeamount;
 		float alpha = startcutout + (1.0f - startcutout) * (amount/maxblood);
-		print (alpha);
-		material.SetFloat ("_Cutoff", alpha);
-
+		//print (alpha);
+		renderer.material.SetFloat ("_Cutoff", alpha);
+		print (renderer.material.GetFloat ("_Cutoff"));
 		if (amount > maxblood) {
-			Destroy(gameObject);
+			//Destroy(gameObject);
+			gameObject.renderer.enabled =false;
 		}
 	}
 
--- Assets/Scripts/JointBreaker.cs
+++ Assets/Scripts/JointBreaker.cs
@@ -6,7 +6,9 @@ public class JointBreaker : MonoBehaviour {
 	public float breakLimit = 100;
 	public float Damage;
 	public ParticleSystem particleeffect_prefab;
+	public BloodStain[] bloodStain_prefab;
 	public GameObject[] enableOnBreak;
+
 	// Use this for initialization
 	void Start () {
 		if (!joint) {
@@ -23,10 +25,32 @@ public class JointBreaker : MonoBehaviour {
 			Debug.DrawRay(contact.point, contact.normal, Color.white);
 
 		}
-		if(collision.contacts[0].otherCollider.gameObject.layer == LayerMask.NameToLayer("Blade")){
+		if (collision.contacts [0].otherCollider.gameObject.layer == LayerMask.NameToLayer ("Blade")) {
 			Damage += collision.relativeVelocity.magnitude * 9;
 			SpawnEffect(collision.contacts[0].point);
+
+			int rndindex = Random.Range (0, bloodStain_prefab.Length);
+			if (rndindex > bloodStain_prefab.Length) {
+					rndindex = bloodStain_prefab.Length;
+			}
+			if (bloodStain_prefab.Length > 0 && bloodStain_prefab [rndindex]) {
+				if (collision.relativeVelocity.magnitude > 0.5f) {
+					Vector3 pos = collision.contacts [0].point;
+					pos.z = bloodStain_prefab [rndindex].gameObject.transform.position.z;
+					pos.y += 0.2f;
+
+					GameObject stain = GameObject.Instantiate (bloodStain_prefab [rndindex].gameObject) as GameObject;
+
+					stain.transform.position = pos;
+					float scl = 0.25f + Random.Range (0, 0.4f);
+					stain.transform.localScale = new Vector3 (scl, scl, scl);
+					stain.transform.eulerAngles = new Vector3 (0f, 0f, Random.value * 40 - 20);
+				}
+			}
+
+
 		}
+			
 		else{
 			Damage += collision.relativeVelocity.magnitude * 0.5f;
 		}

dc3df584f33f4bf90a22ac6f915607e20037286f
Jonne Väisänen <jonne.vaisanen@ludocraft.com>
2015-02-04 22:33:40 +0200

    Fixed some typos from GUI. Adjusted blood stains.

--- Assets/Scripts/GameLogic/GameOverState.cs
+++ Assets/Scripts/GameLogic/GameOverState.cs
@@ -47,6 +47,6 @@ public class GameOverState : GameState
 	public void OnFinish()
 	{
 		MenuManager.Instance.HideMenu<GameOverHUD>();
-		Application.LoadLevel("rigidbody_testbed");
+		Application.LoadLevel("start");
 	}
 }
